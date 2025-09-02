2ccad3570cf80766adc1738b9eba653108b72bbd
Alex Dmiterko <dmiterko.sascha5@gmail.com>
2025-09-02 22:37:58 +0300

    Передана фикстура в тест

--- tests.py
+++ tests.py
@@ -126,8 +126,7 @@ class TestBooksCollector:
     @pytest.mark.parametrize('book_name, book_genre',
                              [['Пикник на обочине', 'Фантастика'], ['451 градус по Фаренгейту', 'Ужасы'],
                               ['12 Стульев', 'Комедии']])
-    def test_delete_book_from_favorites_valid_value_book_in_collection(self, book_name, book_genre):
-        collector = BooksCollector()
+    def test_delete_book_from_favorites_valid_value_book_in_collection(self, book_name, book_genre, collector):
         collector.add_new_book(book_name)
         collector.set_book_genre(book_name, book_genre)
         collector.add_book_in_favorites(book_name)
